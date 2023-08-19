3659920a98d5f75c368ebdd9eb5e014c6f59cc67
yonatanlla <yonatanllanto174@gmail.com>
2023-08-18 23:51:49 -0500

    cambio de deploy back

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import styles from "./App.module.css"
 import axios from "axios"
-axios.defaults.baseURL = "https://pokemons-fullstack-production.up.railway.app";
+axios.defaults.baseURL = "https://pokemons-u47a.onrender.com";
 
 function App() {
   const location = useLocation()
