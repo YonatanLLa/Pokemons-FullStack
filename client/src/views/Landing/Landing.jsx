a71934be6676f83239f98f875e03eefae036741f
yonatanlla <yonatanllanto174@gmail.com>
2023-10-30 10:56:31 -0500

    useEffect

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import styles from "./Landing.module.css";
 import pokebola from "../../assets/pokebola.png";
@@ -6,41 +6,40 @@ import { getPokemons } from "../../redux/action";
 import { useDispatch } from "react-redux";
 
 const Landing = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, [dispatch]);
 
-  	useEffect(() => {
-		dispatch(getPokemons());
-	}, [dispatch]);
-	
-	const handleClick = () => {
-		navigate("/home");
-	};
+  const handleClick = () => {
+    navigate("/home");
+  };
 
-	return (
-		<div className={styles.containerLanding}>
-			<div className={styles.landingImage}>
-				<h1 className={styles.titleLanding}>Pokemon</h1>
-			</div>
-			
-			<div className={styles.childrenLanding}>
-				<h2 className={styles.subtitleLanding}>
-					No hay nada más fuerte que una <br /> amistad forjada en batalla...
-				</h2>
-			</div>
-			<img
-				src={pokebola}
-				alt=""
-				style={{ position: "absolute", left: "30rem", top: "12rem" }}
-			/>
-			<div className={styles.buttonDivLanding}>
-				<button className={styles.buttonLanding} onClick={handleClick}>
-					INGRESAR
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.containerLanding}>
+      <div className={styles.landingImage}>
+        <h1 className={styles.titleLanding}>Pokemon</h1>
+      </div>
+
+      <div className={styles.childrenLanding}>
+        <h2 className={styles.subtitleLanding}>
+          No hay nada más fuerte que una <br /> amistad forjada en batalla...
+        </h2>
+      </div>
+      <img
+        src={pokebola}
+        alt=""
+        style={{ position: "absolute", left: "30rem", top: "12rem" }}
+      />
+      <div className={styles.buttonDivLanding}>
+        <button className={styles.buttonLanding} onClick={handleClick}>
+          INGRESAR
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
