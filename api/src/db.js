f2805e7403fe78b01f9d94357e09d143621bc706
yonatanlla <yonatanllanto174@gmail.com>
2023-11-29 17:52:14 -0500

    cofig db

--- api/src/db.js
+++ api/src/db.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false,  // Puedes ajustar esto según tus necesidades de seguridad
+         }
+      }
    }
 );
 const basename = path.basename(__filename);
