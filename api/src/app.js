bc7de89343dbfc98d583bbf74dac50cafcb0fced
yonatanlla <yonatanllanto174@gmail.com>
2023-11-30 13:14:47 -0500

    resposive

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
 		"Access-Control-Allow-Origin",
-		"https://pokemons-full-stack.vercel.app"
+		"http://localhost:5173"
 	); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
--- api/src/db.js
+++ api/src/db.js
@@ -11,10 +11,12 @@ const sequelize = new Sequelize(
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       dialectOptions: {
-         ssl: {
-            require: true,
-            rejectUnauthorized: false,  // Puedes ajustar esto según tus necesidades de seguridad
-         }
+         ssl: false,
+
+         // ssl: {
+         //    require: true,
+         //    rejectUnauthorized: false,  // Puedes ajustar esto según tus necesidades de seguridad
+         // }
       }
    }
 );
