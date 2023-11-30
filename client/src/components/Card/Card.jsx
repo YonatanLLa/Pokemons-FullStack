9b8e53df7f8319705599be1332a672773fb8fda7
yonatanlla <yonatanllanto174@gmail.com>
2023-11-30 10:11:08 -0500

    cambios

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,34 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
+import Loading from "./Loading";
 // mostrar los pokemons
 
 const Card = ({ name, image, type, id }) => {
+	const [imageLoaded, setImageLoaded] = useState(false)
+
+	console.log(imageLoaded);
+
+	const handleImageLoad = () => {
+		setImageLoaded(true)
+	}
+
+	
 	return (
 		<div className={styles.cardContainer}>
 			<h1 className={styles.cardTitle} style={{ textAlign: "center" }}>
 				{name}
 			</h1>
-			<Link style={{border: "none"}} to={`/detail/${id}`}>
-				<img className={styles.cardImg} src={image} alt="" />
+			<Link style={{ border: "none" }} to={`/detail/${id}`}>
+				<div className={styles.loadingContainer}>
+					{!imageLoaded && <div style={{width: "260px", height: "360px"}}><Loading/></div>}
+						<img
+							className={`${styles.cardImg} ${imageLoaded ? styles.showImage : ""}`}
+							src={image}
+							alt={name}
+							onLoad={handleImageLoad}
+						/>
+				</div>
 			</Link>
 			<div className={styles.cardDetails}>
 				{type.map((tipo, index) => {
---
+++ client/src/components/Card/Loading.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={styles["peeek-loading"]} >
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Loading;
